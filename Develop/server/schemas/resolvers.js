50fb6ccc9a0699cd929a035a55d3c3ef998812a6
Aleenabrink93 <aleenabrink@gmail.com>
2023-09-20 10:15:29 -0600

    updated typeDefs, resolver, server.js and auth.js

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,3 +1,58 @@
-const resolvers = {};
+const {User} = require('../models')
+const {AuthenticationError} = require('apollo-server-express');
+const {signToken} = require('../utils/auth');
+
+const resolvers = {Query:{
+    me: async(parent, args, context)=>{
+        if(context.user){
+            const userData = await User.findOne({_id: context.user._id})
+            return userData;
+        }
+        throw new AuthenticationError('Please login first');
+    }
+},
+Mutation:{
+    addUser: async(parent, args)=>{
+        const user = User.create(args);
+        const token = signToken(user);
+        return{token, user};
+    },
+    login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+        if (!user) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+        const correctPw = await user.isCorrectPassword(password);
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+        const token  =signToken(user);
+        return {token, user};
+    },
+
+    saveBook: async(parent, args, context)=>{
+        if(context.user){
+            const updatedUser = await User.findOneAndUpdate(
+                {_id: context.user._id},
+                {$push: {savedBooks: args}},
+                {new: true}
+            );
+            return updatedUser;
+        }
+        throw new AuthenticationError('Book cannot be added!');
+    },
+
+    removeBook: async (parent, {bookId}, context) =>{
+        if(context.user){
+            const updatedUser = await User.findOneAndUpdate(
+                {_id: context.user._id},
+                {$pull: {savedBooks: {bookId}}},
+                {new: true}
+            );
+            return updatedUser;
+        }
+        throw new AuthenticationError('Book cannot be deleted!');
+    }
+}};
 
 module.exports = resolvers;
--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,30 +1,37 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
-//import Apollo 
-const {ApolloServer} = require('apollo-server-express');
-//import typeDefs and Resolvers
+// import ApolloServer
+const {ApolloServer} = require ('apollo-server-express');
+
+// import typeDefs and resolvers
 const {typeDefs, resolvers} = require('./schemas');
+const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 
-const server = new ApolloServer({typeDefs,resolvers});
-
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-server.applyMiddleware ({app});
+const server = new ApolloServer({typeDefs, resolvers, context:authMiddleware});
 
-app.use(express.urlencoded({ extended: true }));
+server.applyMiddleware({app});
+
+app.use(express.urlencoded({ extended: false }));
+
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  onsole.log (`GraphQL is running at http://localhost:${PORT}${server.graphqlPath }`);
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`GraphQL is running at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
